8ade28367cbf594bb3a847aa355dcc180e2d7d94
Mohak Jain <mohakjain@gmail.com>
2021-04-16 14:34:59 -0700

    code is done!

--- TmCalculator.java
+++ TmCalculator.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
-// import jaligner.Alignment;
-// import jaligner.Sequence;
-// import jaligner.SmithWatermanGotoh;
-// import jaligner.matrix.Matrix;
-// import jaligner.matrix.MatrixLoader;
+import jaligner.Alignment;
+import jaligner.Sequence;
+import jaligner.SmithWatermanGotoh;
+import jaligner.matrix.Matrix;
+import jaligner.matrix.MatrixLoader;
 import java.util.ArrayList;
 
 /**
@@ -22,52 +22,70 @@ public class TmCalculator {
     
     }
 
+    /** Run my temperature checker. 
+     * We take in char[] inputs as the sequence and complementary sequence.
+    */
     public double run(char[] S1, char[] S2) throws IOException, InterruptedException {
       
+        // We specify the location of the folder containing the MELTING5 jar.
         String path_to_melting_executable = "./MELTING5.2.0/executable/";
         
+
+        // I was unable to determine how to run the melting5 jar as a library. 
+        // As such I decided to take this approach which calls the jar file using a java process.
         String[] meltingArgs = {"java", "-jar", "melting5.jar",
                                 "-S",  String.valueOf(S1),
                                 "-H", "dnadna",
-                                "-P", "5e-8",
-                                "-E", 
-                                "Na=0.05", 
+                                "-P", "50e-8",
+                                "-E", "Na=0.05", 
                                 "-C", String.valueOf(S2)
                             };
-        
+        // Arguments above are all defaults. 500nm primer concentration, Na=0.05 looks like the default too.
+
+        // Start a process to deal with MELTING5.
         ProcessBuilder pb = new ProcessBuilder(meltingArgs);
         pb.directory(new File(path_to_melting_executable));
         Process proc = pb.start();
         proc.waitFor();
-        // Then retrieve the process output
         
+        // Then retrieve the process output
         InputStream in = proc.getInputStream();
         InputStream err = proc.getErrorStream();
         
+        // print out the outputs from MELTING5
         byte results[]=new byte[in.available()];
         in.read(results, 0, results.length);        
         System.out.println(new String(results));
         
+        // print out any errors from MELTING5; Not sure if this is necessary.
         byte c[]=new byte[err.available()];
         err.read(c,0,c.length);
         System.out.println(new String(c));
         
-
+        // Process MELTING5's output string:
         String Tm_str = new String(Arrays.copyOfRange(results,  results.length-22, results.length-13));
-        while ( !Character.isDigit(Tm_str.charAt(0)) ) {
+        while ( !(Character.isDigit(Tm_str.charAt(0)) || (Tm_str.charAt(0) == '-')) ) {
             Tm_str = Tm_str.substring(1, Tm_str.length());
+          
         }
 
+
+        // We may error if the 
         if (Tm_str.length() == 0){
             System.out.println("Encountered fatal error!!");
             return 0.0;
         }
 
+        // Return as double the melting temperature.
         double Tm = Double.parseDouble(Tm_str);
         
         return Tm;
     }
 
+    /** MELTING5, the library I employ for this task demands that the complement string is given as a complement.
+     * This function takes in a char[] DNA sequence and returns the complement of it in char[] format. 
+     * Any non-ATCG characters are preserved.
+    */
     public static char[] complement(char[] S) {
         
         char[] S_comp = S.clone();
@@ -93,26 +111,32 @@ public class TmCalculator {
     }
 
     public static void main(String[] args) throws Exception {
-        String s1 = "aaaaaaaaaa-aaaaaaaaaaaaaGAaaaaaaa".toUpperCase();  
-        String s2 = "aaaaaaaaaaaaaaaaaaaaaaaaTaaaaaaaa".toUpperCase();
-
-        // // below deals with jaligner stuff.
-        // Sequence seq1 = new Sequence(s1);
-        // Sequence seq2 = new Sequence(s2);
-
-        // Alignment alignment = SmithWatermanGotoh.align(seq1, seq2, MatrixLoader.load("EDNAFULL_1"), 10f, 0.5f);
+        String s1 = "CCCCCCCCCCCC".toUpperCase();  
+        String s2 = "CCCCCCCCCCCC".toUpperCase();
         
-        // Matrix matrix = alignment.getMatrix();
+        // input dependent: if spaces appear in the sequence we should replace them with hyphens ("-")
+        // s1 = s1.replaceAll("\\s", "-");
+        // s2 = s2.replaceAll("\\s", "-");
+        
+        Sequence seq1 = new Sequence(s1);
+        Sequence seq2 = new Sequence(s2);
 
-        // char[] S1 = alignment.getSequence1();
-        // char[] S2 = alignment.getSequence2();
+        Alignment alignment = SmithWatermanGotoh.align(seq1, seq2, MatrixLoader.load("EDNAFULL_1"), 10f, 0.5f);
+        
+        char[] S1 = alignment.getSequence1();
+        char[] S2 = alignment.getSequence2();
 
-        // comment out below when you can figure out the jaligner stuff:
-        char[] S1 = s1.toCharArray(); 
-        char[] S2 = s2.toCharArray();
+        // // comment out below when you can figure out the jaligner stuff:
+        // char[] S1 = s1.toCharArray(); 
+        // char[] S2 = s2.toCharArray();
         
         // Complement S2 because that's what MELTING likes:
         S2 = complement(S2);
+
+        // Print what the alignments actually look like:
+        System.out.println(S1);
+        System.out.println(S2);
+        
         TmCalculator tmCalculator = new TmCalculator();
         tmCalculator.initiate();
         double result = tmCalculator.run(S1, S2);
